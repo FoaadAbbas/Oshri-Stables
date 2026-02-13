a7a9d25210e2ca9dc5184dc97f910ddc7ba9ee2f
FoaadAbbas <157692834+FoaadAbbas@users.noreply.github.com>
2026-02-13 18:21:44 +0200

    hide api keys

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBY0qdTeF2azQodjEuM5eh_RrQe-AaG7JE",
-    authDomain: "horsedashboard.firebaseapp.com",
-    projectId: "horsedashboard",
-    storageBucket: "horsedashboard.firebasestorage.app",
-    messagingSenderId: "697763821845",
-    appId: "1:697763821845:web:6ca9d4035721a50d38511c",
-    measurementId: "G-8D9VR99Y05"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
